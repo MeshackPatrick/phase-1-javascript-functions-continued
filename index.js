e15039a04f26aefd86e7bc03dd86ff3bd30832c5
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-16 16:50:15 +0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${action}!`;
+  }
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  function wrapAdjective(message = "*") {
+    return function (adjective = "special") {
+      return `You are ${message}${adjective}${message}!`;
+    }
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("%");
+  console.log(encouragingPromptFunction("a dedicated programmer"));
+  console.log(encouragingPromptFunction());
+  
